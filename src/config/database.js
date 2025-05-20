f067ca4abe168ab5f839362124bb7e94e2c21b68
VicenteAntonio <vicente.antonio@uc.cl>
2025-05-20 11:51:08 -0400

    feat(database): Mejora la configuración de la base de datos para cargar variables de entorno desde un archivo específico y agrega un registro de las variables de entorno. feat(seeders): Actualiza los seeders de libros con nuevos títulos y autores, mejorando la diversidad del catálogo.

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,12 @@
-require("dotenv").config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
+console.log("ENV Variables:", {
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  DB_HOST: process.env.DB_HOST
+});
 
 module.exports = {
   development: {
